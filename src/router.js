ede98791e09331c1269e7a040c29c27da4582afb
Vikasjawla <vikasjawla.stintlief@gmail.com>
2022-03-23 12:56:15 +0530

    Add: Vue + axios + router

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Router from "vue-router";
+Vue.use(Router);
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      alias: "/books",
+      name: "books",
+      component: () => import("./components/BookList")
+    },
+    {
+      path: "/books/:id",
+      name: "book-details",
+      component: () => import("./components/Book")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/AddBook")
+    }
+  ]
+});
